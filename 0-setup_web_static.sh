04a3ecceb60143299e2bb2014ab67cfa393720a2
stephenchuks <104122101+stephenchuks@users.noreply.github.com>
2023-04-08 13:19:36 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
+# This Bash script sets up your web servers for the deployment of AirBNB web_static
 
-# Install Nginx if it's not already installed
-if ! [ -x "$(command -v nginx)" ]; then
-  apt-get update
-  apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
 
-# Create necessary directories
-mkdir -p /data/web_static/{releases/test,shared}
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create a symbolic link
-if [ -h /data/web_static/current ]; then
-  unlink /data/web_static/current
-fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Give ownership of the /data/ folder to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
-
-# Update the Nginx configuration file
-sed -i '/^\tserver_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-
-# Restart Nginx
 service nginx restart
-
-exit 0
-
 
